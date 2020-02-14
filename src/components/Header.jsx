0c9624afe0f4776cc60161afb5f884d8d41c7acc
Student <example@example.com>
2020-02-14 15:53:34 -0800

    Add rough nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
   return (
-    <h1>Tapper</h1>
+    <div>
+      <h1>Tapper</h1>
+      <Link to="/">Home</Link> | <Link to="/NewKegForm">Add New Keg</Link>
+    </div>  
   );
 }
 
