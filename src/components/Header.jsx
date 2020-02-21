1eeab68253d50722b17fc9a0091620bc1b8b02a3
Drew Peterson <drew.a.peterson@hotmail.com>
2020-02-21 10:01:29 -0800

    Add banner image to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import tapperlogo from '../assets/images/tapper-logo.jpg';
 
 
 function Header() {
+  const bannerStyle = {
+    width: '80%'
+  }
   return (
     <div>
-      <h1>Tapper</h1>
+      <img style={bannerStyle} src={tapperlogo} />
+      <hr/>
       <Link to='/'>Home</Link> | <Link to='/NewKegForm'>Add New Keg</Link>
     </div>
   );
